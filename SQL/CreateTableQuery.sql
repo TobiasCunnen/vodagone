a14a45964802feebd7b8c487306112d1e76e37c7
Tobias Cunnen <t.cunnen@gmail.com>
2018-03-24 00:27:34 +0100

    Added a table to the db

--- SQL/CreateTableQuery.sql
+++ SQL/CreateTableQuery.sql
@@ -1,15 +1,14 @@
 use vodagoneDB;
 
+drop table AbonnementenVanAbonnees;
+drop table Abonnementen;
+drop table Abonnees;
+
 create table Abonnementen
 (
 id int primary key auto_increment,
 aanbieder varchar(255) not null,
-dienst varchar(255) not null,
-prijs decimal(9,2),
-startDatum date,
-verdubbeling varchar(255),
-deelbaar boolean,
-status varchar(255)
+dienst varchar(255) not null
 );
 
 create table Abonnees
@@ -19,3 +18,21 @@ name varchar(255) unique key not null,
 email varchar(255) not null,
 wachtwoord varchar(255) not null
 );
+
+create table AbonnementenVanAbonnees
+(
+abonneesId int,
+abonnementId int,
+aanbieder varchar(255) not null,
+dienst varchar(255) not null,
+prijs decimal(9,2),
+startDatum date,
+verdubbeling varchar(255),
+deelbaar boolean,
+status varchar(255),
+
+primary key(abonneesId,abonnementId),
+FOREIGN KEY (abonneesId) REFERENCES Abonnees(id),
+FOREIGN KEY (abonnementId) REFERENCES Abonnementen(id)
+);
+
--- SQL/InsertQuery.sql
+++ SQL/InsertQuery.sql
@@ -1,5 +1,9 @@
 use vodagoneDB;
 
+delete from abonnementenVanAbonnees;
+delete from abonnees;
+delete from abonnementen;
+
 insert into abonnees (name, email, wachtwoord)
 values
 ('Tobias Cunnenn','tobiascunnen@gmail.com','wachtwoord'),
@@ -7,8 +11,16 @@ values
 ('Dennis Breuker','Dennis.Breuker@han.nl','wachtwoord'),
 ('Michel Portier','Michel.Portier@han.nl','wachtwoord');
 
-insert into abonnementen (aanbieder,dienst,prijs,startDatum,verdubbeling,deelbaar,status)
+insert into abonnementen (aanbieder,dienst)
 values
-('vodafone', 'Mobiele telefonie 100', 5.00, '2017-01-01', 'standaard', true, 'actief'),
-('vodafone', 'Mobiele telefonie 250', 5.00, '2017-01-01' , 'standaard', true, 'actief'),
-('ziggo', 'Kabel-internet (download 300 Mpbs)', 5.00, '2017-01-01', 'standaard', true, 'actief');
+('vodafone', 'Mobiele telefonie 100'),
+('vodafone', 'Mobiele telefonie 250'),
+('ziggo', 'Kabel-internet (download 300 Mpbs)'),
+('ziggo', 'Glasvezel-internet (download 500 Mbps)');
+
+insert into abonnementenVanAbonnees (abonneesId, abonnementId,aanbieder,dienst,prijs,startDatum,verdubbeling,deelbaar,status)
+values
+(1,1,'vodafone', 'Mobiele telefonie 100', 5.00, '2017-01-01', 'standaard', true, 'actief'),
+(1,2,'vodafone', 'Mobiele telefonie 250', 5.00, '2017-01-01' , 'standaard', true, 'actief'),
+(1,3,'ziggo', 'Kabel-internet (download 300 Mpbs)', 5.00, '2017-01-01', 'standaard', true, 'actief');
+
